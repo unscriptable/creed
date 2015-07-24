28984a7cbf84c7883235dd5144652377c0b6a648
Brian Cavalier <brian@hovercraftstudios.com>
2015-07-23 22:49:12 -0400

    Playing with algebra

--- src/Promise.js
+++ src/Promise.js
@@ -8,6 +8,9 @@ import { PENDING, FULFILLED, REJECTED, NEVER } from './state';
 import { isFulfilled, isSettled } from './inspect';
 
 import then from './then';
+import _delay from './delay';
+import _timeout from './timeout';
+import map from './map';
 
 import Race from './Race';
 import Merge from './Merge';
@@ -268,6 +271,18 @@ class Never {
 }
 Never.prototype._isPromise = marker;
 
+Future.prototype.map =
+Fulfilled.prototype.map = function(f) {
+    return map(f, this, new Future());
+};
+
+Rejected.prototype.map =
+Never.prototype.map = returnThis;
+
+function returnThis() {
+    return this;
+}
+
 // -------------------------------------------------------------
 // ## Creating promises
 // -------------------------------------------------------------
@@ -373,3 +388,40 @@ class Continuation {
         this.ref._runAction(this.action);
     }
 }
+
+// -------------------------------------------------------------
+// ## ES6 Promise polyfill
+// -------------------------------------------------------------
+
+const NOARGS = [];
+
+// Promise :: ((a -> ()) -> (e -> ())) -> Promise e a
+class CreedPromise extends Future {
+    constructor(f) {
+        super();
+        runResolver(f, void 0, NOARGS, this);
+    }
+}
+
+CreedPromise.resolve = resolve;
+CreedPromise.reject = reject;
+CreedPromise.all = all;
+CreedPromise.race = race;
+
+export function shim() {
+    let orig = typeof Promise === 'function' && Promise;
+
+    if (typeof self !== 'undefined') {
+        self.Promise = CreedPromise;
+    } else if (typeof global !== 'undefined') {
+        global.Promise = CreedPromise;
+    }
+
+    return orig;
+}
+
+if (typeof Promise !== 'function') {
+    shim();
+}
+
+export { CreedPromise as Promise };
